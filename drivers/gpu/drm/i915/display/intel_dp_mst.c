82db937196d665387c57e774204f5ab5d148c870
Imre Deak <imre.deak@intel.com>
2024-12-12 01:03:22 +0200

    drm/i915/dp_mst: Expose a connector to kernel users after it's properly initialized
    After a connector is added to the drm_mode_config::connector_list, it's
    visible to any in-kernel users looking up connectors via the above list.
    Make sure that the connector is properly initialized before such
    look-ups, by initializing the connector with
    drm_connector_dynamic_init() - which doesn't add the connector to the
    list - and registering it with drm_connector_dynamic_register() - which
    adds the connector to the list - after the initialization is complete.
    
    v2:
    - Rebase on the change which moves adding the connector to the
      connector list only later when calling
      drm_connector_dynamic_register().
    
    v3:
    - Rebase on drm-misc-next, due to a trivial conflict with
      commit 5503f8112e52 ("drm/i915/mst: unify MST topology callback naming ..."),
      which is only in drm-intel-next.
    - Fix s/drm_connector_dynamic_register()/drm_connector_dynamic_init()
      typo in the commit log.
    
    Cc: Lyude Paul <lyude@redhat.com>
    Reviewed-by: Rodrigo Vivi <rodrigo.vivi@intel.com> (v1)
    Reviewed-by: Lyude Paul <lyude@redhat.com>
    Reviewed-by: Jani Nikula <jani.nikula@intel.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241211230328.4012496-6-imre.deak@intel.com

--- drivers/gpu/drm/i915/display/intel_dp_mst.c
+++ drivers/gpu/drm/i915/display/intel_dp_mst.c
@@ -1704,6 +1704,8 @@ mst_topology_add_connector(struct drm_dp_mst_topology_mgr *mgr,
 	if (!intel_connector)
 		return NULL;
 
+	connector = &intel_connector->base;
+
 	intel_connector->get_hw_state = mst_connector_get_hw_state;
 	intel_connector->sync_state = intel_dp_connector_sync_state;
 	intel_connector->mst_port = intel_dp;
@@ -1722,19 +1724,18 @@ mst_topology_add_connector(struct drm_dp_mst_topology_mgr *mgr,
 	 */
 	intel_connector->base.dev = mgr->dev;
 
-	intel_connector->dp.dsc_decompression_aux = drm_dp_mst_dsc_aux_for_port(port);
-	intel_dp_mst_read_decompression_port_dsc_caps(intel_dp, intel_connector);
-	intel_connector->dp.dsc_hblank_expansion_quirk =
-		detect_dsc_hblank_expansion_quirk(intel_connector);
-
-	connector = &intel_connector->base;
-	ret = drm_connector_init(display->drm, connector, &mst_connector_funcs,
-				 DRM_MODE_CONNECTOR_DisplayPort);
+	ret = drm_connector_dynamic_init(display->drm, connector, &mst_connector_funcs,
+					 DRM_MODE_CONNECTOR_DisplayPort, NULL);
 	if (ret) {
 		drm_dp_mst_put_port_malloc(port);
 		intel_connector_free(intel_connector);
 		return NULL;
 	}
+
+	intel_connector->dp.dsc_decompression_aux = drm_dp_mst_dsc_aux_for_port(port);
+	intel_dp_mst_read_decompression_port_dsc_caps(intel_dp, intel_connector);
+	intel_connector->dp.dsc_hblank_expansion_quirk =
+		detect_dsc_hblank_expansion_quirk(intel_connector);
 
 	drm_connector_helper_add(connector, &mst_connector_helper_funcs);
 
