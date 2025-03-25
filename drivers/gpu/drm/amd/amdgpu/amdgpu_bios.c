17c647371cb39aa966b1d1a9cd15cf20d3a8927c
Lijo Lazar <lijo.lazar@amd.com>
2025-03-25 11:42:08 +0530

    drm/amdgpu: Prefer shadow rom when available
    Fetch VBIOS from shadow ROM when available before trying other methods
    like EFI method.
    
    Signed-off-by: Lijo Lazar <lijo.lazar@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Fixes: 9c081c11c621 ("drm/amdgpu: Reorder to read EFI exported ROM first")
    Closes: https://gitlab.freedesktop.org/drm/amd/-/issues/4066
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Cc: stable@vger.kernel.org

--- drivers/gpu/drm/amd/amdgpu/amdgpu_bios.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_bios.c
@@ -454,6 +454,21 @@ success:
 	return true;
 }
 
+static bool amdgpu_prefer_rom_resource(struct amdgpu_device *adev)
+{
+#ifdef __linux__
+	struct resource *res = &adev->pdev->resource[PCI_ROM_RESOURCE];
+
+	return (res->flags & IORESOURCE_ROM_SHADOW);
+#elif defined(__FreeBSD__)
+	/*
+	 * This is the same logic as we have in `vga_pci_map_bios()` to
+	 * determine if the shadow copy of the Video BIOS should be used.
+	 */
+	return vga_pci_is_boot_display(device_get_parent(adev->pdev->dev.bsddev));
+#endif
+}
+
 static bool amdgpu_get_bios_dgpu(struct amdgpu_device *adev)
 {
 	if (amdgpu_atrm_get_bios(adev)) {
@@ -472,14 +487,27 @@ static bool amdgpu_get_bios_dgpu(struct amdgpu_device *adev)
 		goto success;
 	}
 
-	if (amdgpu_read_platform_bios(adev)) {
-		dev_info(adev->dev, "Fetched VBIOS from platform\n");
-		goto success;
-	}
+	if (amdgpu_prefer_rom_resource(adev)) {
+		if (amdgpu_read_bios(adev)) {
+			dev_info(adev->dev, "Fetched VBIOS from ROM BAR\n");
+			goto success;
+		}
 
-	if (amdgpu_read_bios(adev)) {
-		dev_info(adev->dev, "Fetched VBIOS from ROM BAR\n");
-		goto success;
+		if (amdgpu_read_platform_bios(adev)) {
+			dev_info(adev->dev, "Fetched VBIOS from platform\n");
+			goto success;
+		}
+
+	} else {
+		if (amdgpu_read_platform_bios(adev)) {
+			dev_info(adev->dev, "Fetched VBIOS from platform\n");
+			goto success;
+		}
+
+		if (amdgpu_read_bios(adev)) {
+			dev_info(adev->dev, "Fetched VBIOS from ROM BAR\n");
+			goto success;
+		}
 	}
 
 	if (amdgpu_read_bios_from_rom(adev)) {
